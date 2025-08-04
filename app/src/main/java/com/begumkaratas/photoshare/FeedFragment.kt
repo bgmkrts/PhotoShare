a8776976ed20c4c8281d7b1a61053fc84a67b846
Begüm Karataş <begumkaratas18@gmail.com>
2025-08-04 22:35:18 +0300

    Feed Interface integrated

--- app/src/main/java/com/begumkaratas/photoshare/FeedFragment.kt
+++ app/src/main/java/com/begumkaratas/photoshare/FeedFragment.kt
@@ -5,23 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.begumkaratas.photoshare.databinding.FragmentFeedBinding
+import com.begumkaratas.photoshare.databinding.FragmentKullaniciBinding
 
 
 class FeedFragment : Fragment() {
-
+    private var _binding: FragmentFeedBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false)
+        _binding = FragmentFeedBinding.inflate(inflater, container, false)
+        return binding.root    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
-
 
 }
