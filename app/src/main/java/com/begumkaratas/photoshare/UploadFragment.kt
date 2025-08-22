bdaf4724a63a117918e91e2c5dd48e4b222dda8e
Begüm Karataş <begumkaratas18@gmail.com>
2025-08-22 14:55:09 +0300

    menu Checking permissions Finishing permissions

--- app/src/main/java/com/begumkaratas/photoshare/UploadFragment.kt
+++ app/src/main/java/com/begumkaratas/photoshare/UploadFragment.kt
@@ -1,12 +1,27 @@
 package com.begumkaratas.photoshare
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.begumkaratas.photoshare.databinding.FragmentFeedBinding
 import com.begumkaratas.photoshare.databinding.FragmentUploadBinding
+import com.google.android.material.snackbar.Snackbar
+import android.Manifest
+import android.app.Activity.RESULT_OK
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 
 
 class UploadFragment : Fragment() {
@@ -14,15 +29,21 @@ class UploadFragment : Fragment() {
 
     private var _binding: FragmentUploadBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    var secilenGorsel: Uri? = null
+    var secilenBitmap: Bitmap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        registerLauncher()
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.yukleButton.setOnClickListener { yukleTiklandi(it) }
-        binding.imageView.setOnClickListener {gorselSec(it)  }
+        binding.imageView.setOnClickListener { gorselSec(it) }
 
     }
 
@@ -33,18 +54,108 @@ class UploadFragment : Fragment() {
         _binding = FragmentUploadBinding.inflate(inflater, container, false)
         return binding.root
     }
-    fun yukleTiklandi(view: View){
+
+    fun yukleTiklandi(view: View) {
 
     }
 
-    fun gorselSec(view: View){
+    fun gorselSec(view: View) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            //Read media images
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.READ_MEDIA_IMAGES
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                //izin verilmediyse
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        requireActivity(),
+                        Manifest.permission.READ_MEDIA_IMAGES
+                    )
+                ) {
+                    Snackbar.make(
+                        view,
+                        "Galeriye gitmek için izin vermeniz gerekiyor",
+                        Snackbar.LENGTH_INDEFINITE
+                    ).setAction("İzin Ver",
+
+                        View.OnClickListener {
+                            //İzin istememiz lazım
+                            permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                        }).show()
+
+                } else {
+                    //izin istememiz lazım
+                    permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+
+                }
+            } else {
+                //izin zaten verildiyse
+                //galeriye git
+                val intentToGallery=Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+
+
+            }
+        } else {
+            //read external storage
+        }
+
 
     }
+
+    private fun registerLauncher() {
+
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    val intentFromResult = result.data
+                    if (intentFromResult != null) {
+                        secilenGorsel = intentFromResult.data
+                        try {
+                            if (Build.VERSION.SDK_INT >= 28) {
+                                val source = ImageDecoder.createSource(
+                                    requireActivity().contentResolver,
+                                    secilenGorsel!!
+                                )
+                                secilenBitmap = ImageDecoder.decodeBitmap(source)
+                                binding.imageView.setImageBitmap(secilenBitmap)
+                            } else {
+                                secilenBitmap = MediaStore.Images.Media.getBitmap(
+                                    requireActivity().contentResolver,
+                                    secilenGorsel
+                                )
+                                binding.imageView.setImageBitmap(secilenBitmap)
+
+                            }
+
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+
+                    }
+                }
+            }
+        permissionLauncher=registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            result->
+            if(result){
+                //izin verildi
+                val intentToGallery=Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+
+            }else{
+                //kullanıcı izni reddetti
+                Toast.makeText(requireContext(),"İzni reddettiniz,izne ihtiyacımız var",Toast.LENGTH_LONG).show()
+
+            }
+        }
+
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 
 
 }
